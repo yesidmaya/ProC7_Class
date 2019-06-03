1d5099b1c0dd1350c438a14cbba51131ee91b31d
Alejandro <yesidmaya@gmail.com>
2019-06-03 14:24:55 -0500

    Class

---
+++ Proyecto7/Fibonacci.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Proyecto7
+{
+    class Fibonacci
+    {
+        int num1 = 0;
+        int num2 = 0;
+
+        public Fibonacci() //Constructor sin parametros siempre tiene que estar
+        {
+            
+        }
+
+        public Fibonacci( int a, int b) //Constructor con parametros
+        {
+            num1 = a;
+            num2 = b;
+        }
+
+        public int validarNumero(string valor)
+        {
+            int numero = 0;
+            try
+            {
+                numero = Convert.ToInt32(valor); // convertimos valor a entero
+            }
+            catch (Exception /*ex*/)
+            {
+                Console.WriteLine("POR FAVOR DIGITA UN NUMERO" /*+ ex.ToString()*/); //ex para mostrar mas detalle del error
+                Console.ReadLine();
+            }
+            return numero;
+        }
+
+        public string calcularFibonacci(int numero)
+        {
+            string mostrar = string.Empty;
+            int suma = 0;
+            while (suma <= numero)
+            {
+                suma = num1 + num2;
+                num1 = num2;
+                num2 = suma;
+                mostrar = mostrar + " " + suma;
+            }
+            return mostrar;
+        }
+    }
+}
---
+++ Proyecto7/Program.cs
@@ -0,0 +1,40 @@
+﻿using Proyecto7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto5_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*********************** Serie Fibonacci: 0,1,1,2,3,5,8,13,21,34,55,89....
+
+            int numero = -1;
+            int a = 0;
+            int b = 1;
+            string mostrarPantalla = String.Empty;// almacenamos la cadena de texto y colocamos la cadena vacia
+            mostrarPantalla = "0";
+
+            Console.WriteLine("Introduzca valor FINAL de la serie FIBONACCI ->");
+            string valor = Console.ReadLine(); // tomamos valor
+
+            Proyecto7.Fibonacci fib = new Fibonacci(a, b); // llamamos a cla CLASS con constructor de clase
+                                                           // se lo puede simplificar asi Fibonacci fib = new Fibonacci();
+
+            numero = fib.validarNumero(valor); // instanciamos el objeto de la CLASS
+            mostrarPantalla = fib.calcularFibonacci(numero); // instanciamos el objeto de la CLASS
+
+
+            //mostrarPantalla = mostrarPantalla + calcularFibonacci(a, b, numero); // llamamos al METODO calcularFibonacci
+
+            Console.WriteLine(mostrarPantalla);
+            Console.ReadLine();
+        }
+
+        
+    }
+}
